17468c4682615a02821cbbe1979550ac08769d5e
MaiteAyala <maiteab67@gmail.com>
2025-10-22 10:10:06 -0500

    Delete ReservaDeHotel directory

--- ReservaDeHotel/src/main/java/com/ReservaHoteles/Repository/HabitacionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ReservaHoteles.Repository;
-
-import com.ReservaHoteles.model.Habitacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HabitacionRepository extends JpaRepository<Habitacion, Integer> {
-}
--- ReservaDeHotel/src/main/java/com/ReservaHoteles/Repository/RolRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ReservaHoteles.Repository;
-
-import com.ReservaHoteles.model.Roles;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolRepository extends JpaRepository<Roles, Integer> {
-}
